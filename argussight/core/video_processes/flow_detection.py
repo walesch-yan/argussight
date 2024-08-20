e0943fe08355c225e41774b6f432f807d06347df
walesch-yan <yan.walesch@esrf.fr>
2024-08-20 14:05:22 +0200

    moved configuration files into folder

--- argussight/core/video_processes/flow_detection.py
+++ argussight/core/video_processes/flow_detection.py
@@ -52,7 +52,7 @@ class FlowDetection(Vprocess):
         
     def load_params(self) -> None:
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        params_file = os.path.join(current_dir, 'flow_detection.yaml')
+        params_file = os.path.join(current_dir, '../configurations/flow_detection.yaml')
         with open(params_file, 'r') as file:
             params = yaml.safe_load(file)
         
